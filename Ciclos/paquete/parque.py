ede477822a8f07331698f73be552fbd9e30e4ef2
lautaro cardozo <lautycardozo01@gmail.com>
2025-08-29 11:30:40 -0300

    Entrega de Ciclos

--- Ciclos/paquete/parque.py
+++ Ciclos/paquete/parque.py
@@ -3,9 +3,12 @@ def mostrar_atracciones():
     carrusel = None
     montania_rusa = None
     casa_del_terror = None
+    descuento = False
     nombre = input("Ingrese su nombre: ")
     edad = int(input("Ingrese su edad: "))
     cantidad_atracciones = int(input("A cuantas atracciones quiere subir?: "))
+    if cantidad_atracciones >= 3:
+        descuento = True
     total = 0
     for _ in range(cantidad_atracciones):
         atraccion = input("A que atraccion quiere subir?: ")
@@ -16,7 +19,8 @@ def mostrar_atracciones():
             montania_rusa = puede_subir(edad,atraccion)
         else:
             casa_del_terror = puede_subir(edad,atraccion)
-            
+    if descuento:
+        total = total * 0.9
     mostrar_resumen(edad,nombre,carrusel,montania_rusa,casa_del_terror,total)
 
 def puede_subir(edad,atraccion):
@@ -46,7 +50,7 @@ def calcular_precio(atraccion:str):
         retorno = 1500
     return retorno
 
-def mostrar_resumen(edad,nombre,carrusel,montania_rusa,casa_del_terror,total):    
+def mostrar_resumen(edad,nombre,carrusel,montania_rusa,casa_del_terror,total,descuento):    
     print(f"Visitante {nombre}, edad {edad}")
     if carrusel:
         print("Pudo subir al carrusel")
@@ -60,4 +64,7 @@ def mostrar_resumen(edad,nombre,carrusel,montania_rusa,casa_del_terror,total):
         print("Pudo subir a la casa del terror")
     else:
         print("No pudo subir a la casa del terror")
-    print(f"El total es de {total}")
+    if descuento:
+        print(f"El total con descuento es de = {total}")
+    else:
+        print(f"El total es de = {total}")
